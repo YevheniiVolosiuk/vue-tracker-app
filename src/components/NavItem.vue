77ac3ee8f690aea0e7ba7dd80de20bacf045a6a2
Yevhenii_dev <77569229+Yevhenii-Web-dev@users.noreply.github.com>
2023-04-16 15:12:55 +0200

    refactored Nav component

---
+++ src/components/NavItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <li class="flex-1">
+        <a
+            v-bind="$attrs"
+            class="flex flex-col items-center p-2 text-xs capitalize"
+        >
+            <slot />
+        </a>
+    </li>
+</template>
+
+<script>
+export default {
+    inheritAttrs: false,
+};
+</script>
--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,30 +1,26 @@
 <template>
     <nav class="sticky bottom-0 z-20">
         <ul class="flex items-center justify-around border-t">
-            <li
-                v-for="(icon, navName) in navIcons"
-                :key="navName"
-                class="flex-1"
+            <NavItem
+                v-for="(icon, page) in navItems"
+                :key="page"
+                :href="`#${page}`"
             >
-                <a
-                    :href="`#${navItem}`"
-                    class="flex flex-col items-center p-2 text-xs capitalize"
-                >
-                    <component
-                        :is="icon"
-                        class="h-6 w-6"
-                    />
-                    {{ navName }}
-                </a>
-            </li>
+                <component
+                    :is="icon"
+                    class="h-6 w-6"
+                />
+                {{ page }}
+            </NavItem>
         </ul>
     </nav>
 </template>
 
 <script setup>
-import {ClockIcon, ListBulletIcon, ChartBarIcon} from '@heroicons/vue/24/outline';
+import {ClockIcon, ListBulletIcon, ChartBarIcon} from "@heroicons/vue/24/outline";
+import NavItem from "./NavItem.vue";
 
-const navIcons = {
+const navItems = {
     timeline: ClockIcon,
     activities: ListBulletIcon,
     progress: ChartBarIcon,
