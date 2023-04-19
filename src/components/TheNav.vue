41a023155229b772c3709c09920558356a12221f
Yevhenii_dev <77569229+Yevhenii-Web-dev@users.noreply.github.com>
2023-04-20 01:01:52 +0200

    normalize hash navItems

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -5,7 +5,7 @@
                 v-for="(icon, page) in navItems"
                 :key="page"
                 :href="`#${page}`"
-                :class="{'bg-gray-100 pointer-events-none' : activePage === page}"
+                :class="{'pointer-events-none bg-gray-100': activePage === page}"
                 @click="activePage = page"
             >
                 <component
@@ -19,17 +19,27 @@
 </template>
 
 <script setup>
-import { ClockIcon, ListBulletIcon, ChartBarIcon } from "@heroicons/vue/24/outline";
-import { ref } from 'vue';
+import {ClockIcon, ListBulletIcon, ChartBarIcon} from "@heroicons/vue/24/outline";
+import {ref} from "vue";
+import {PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS} from "../constants.js";
 import NavItem from "./NavItem.vue";
 
 const navItems = {
-    timeline: ClockIcon,
-    activities: ListBulletIcon,
-    progress: ChartBarIcon,
+    [PAGE_TIMELINE]: ClockIcon,
+    [PAGE_ACTIVITIES]: ListBulletIcon,
+    [PAGE_PROGRESS]: ChartBarIcon,
 };
 
-const activePage = ref('timeline');
+const activePage = ref(getActiveHashPage());
 
+function getActiveHashPage() {
+    const hash = window.location.hash.slice(1);
+    if (Object.keys(navItems).includes(hash)) {
+        return hash;
+    }
 
+    window.location.hash = PAGE_TIMELINE;
+
+    return PAGE_TIMELINE;
+}
 </script>
