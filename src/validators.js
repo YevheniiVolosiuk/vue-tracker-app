4522ac8b7ac15587dbd4775e1cee8adb12a95659
Yevhenii_dev <77569229+Yevhenii-Web-dev@users.noreply.github.com>
2023-05-04 10:55:50 +0200

    refactored validators file

--- src/validators.js
+++ src/validators.js
@@ -8,15 +8,48 @@ export function validateTimelineItems(timelineItems) {
     return timelineItems.every(isTimelineItemValid);
 }
 
-export function isTimelineItemValid({hour}) {
-    return typeof hour === "number" && hour >= MIDNIGHT_HOUR && hour < HOURS_IN_DAY;
+export function isTimelineItemValid({ hour }) {
+    return isHourValid(hour);
 }
 
 export function validateSelectOptions(options) {
     return options.every(isSelectOptionValid);
 }
 
-function isSelectOptionValid({value, label}) {
-    return typeof value === "number" && typeof label === "string";
+export function isUndefinedOrNull(value) {
+    return isUndefined(value) || isNull(value);
 }
+
+export function isNumberOrNull(value) {
+    return isNumber(value) || isNull(value);
+}
+
+function isSelectOptionValid({value, label}) {
+    return isNumber(value) && isString(label);
+}
+
+function isHourValid(hour) {
+    return isNumber(hour) && isBetween(hour, MIDNIGHT_HOUR, HOURS_IN_DAY - 1);
+}
+
+function isBetween(value, start, end) {
+    return value >= start && value <= end;
+}
+
+function isUndefined(value) {
+    return typeof value === "undefined";
+}
+
+function isNull(value) {
+    return value === null;
+}
+
+function isNumber(value) {
+    return typeof value === "number";
+}
+
+function isString(value) {
+    return typeof value === "string";
+}
+
 
