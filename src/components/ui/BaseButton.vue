3b2232fe1ba9c47e2f7030d36c64afa04c1fddeb
Yevhenii Volosiuk <volosiukweb@gmail.com>
2023-05-09 13:26:38 +0200

    refactored basebutton component

--- src/components/ui/BaseButton.vue
+++ src/components/ui/BaseButton.vue
@@ -1,9 +1,31 @@
 <template>
-    <button
-        class="rounded bg-gray-100 p-3 enabled:hover:bg-gray-200 disabled:cursor-not-allowed disabled:opacity-50"
-    >
+    <button :class="BaseButtonClasses">
         <slot></slot>
     </button>
 </template>
 
-<script setup></script>
+<script setup>
+import {computed} from "vue";
+import {cva} from "class-variance-authority";
+import { BUTTON_TYPE_NEUTRAL, BUTTON_TYPE_DANGER } from "@/constants";
+import { isButtonTypeValid } from "@/validators";
+
+const props = defineProps({
+    type: {
+        type: String,
+        default: BUTTON_TYPE_NEUTRAL,
+        validator: isButtonTypeValid
+    },
+});
+
+const BaseButtonClasses = computed(() => {
+    return cva("rounded p-3 disabled:cursor-not-allowed disabled:opacity-50", {
+        variants: {
+            type: {
+                [BUTTON_TYPE_NEUTRAL]: "bg-gray-100 enabled:hover:bg-gray-200",
+                [BUTTON_TYPE_DANGER]: "bg-red-100 enabled:hover:bg-red-600 text-white",
+            },
+        },
+    })({type: props.type});
+});
+</script>
--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,31 @@
 import {ClockIcon, ListBulletIcon, ChartBarIcon} from "@heroicons/vue/24/outline";
 
+//---------------------------------------------------------------- Time Constants
+export const HOURS_IN_DAY = 24;
+export const MIDNIGHT_HOUR = 0;
+
+//---------------------------------------------------------------- Activity Constants
+export const ACTIVITIES = ["Coding", "Reading", "Training"];
+
+export const PERIOD_SELECT_OPTIONS = [
+    {value: 15, label: "0:15"},
+    {value: 30, label: "0:30"},
+    {value: 45, label: "0:45"},
+];
+
+//---------------------------------------------------------------- Timeline Constsns
 export const PAGE_TIMELINE = "timeline";
 export const PAGE_ACTIVITIES = "activities";
 export const PAGE_PROGRESS = "progress";
-
-export const HOURS_IN_DAY = 24;
-export const MIDNIGHT_HOUR = 0;
 
 export const NAV_ITEMS = {
     [PAGE_TIMELINE]: ClockIcon,
     [PAGE_ACTIVITIES]: ListBulletIcon,
     [PAGE_PROGRESS]: ChartBarIcon,
 };
+
+//---------------------------------------------------------------- Ui Constants
+export const BUTTON_TYPE_NEUTRAL = "neutral";
+export const BUTTON_TYPE_DANGER = "danger";
+
+export const BUTTON_TYPES = [BUTTON_TYPE_NEUTRAL, BUTTON_TYPE_DANGER];
