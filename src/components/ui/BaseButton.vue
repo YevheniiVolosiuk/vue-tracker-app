b66cee9f98be24d362203696a0e20e1b891a0d0a
Yevhenii Volosiuk <volosiukweb@gmail.com>
2023-05-09 18:09:55 +0200

    fixed rendering BaseButton from 30 times to 1 time in app

--- src/components/ui/BaseButton.vue
+++ src/components/ui/BaseButton.vue
@@ -1,31 +1,31 @@
 <template>
-    <button :class="BaseButtonClasses">
+    <button :class="BaseButtonClasses({type})">
         <slot></slot>
     </button>
 </template>
 
-<script setup>
-import {computed} from "vue";
+<script>
+import {BUTTON_TYPE_NEUTRAL, BUTTON_TYPE_DANGER} from "@/constants";
 import {cva} from "class-variance-authority";
-import { BUTTON_TYPE_NEUTRAL, BUTTON_TYPE_DANGER } from "@/constants";
-import { isButtonTypeValid } from "@/validators";
 
-const props = defineProps({
+const BaseButtonClasses = cva("rounded p-3 disabled:cursor-not-allowed disabled:opacity-50", {
+    variants: {
+        type: {
+            [BUTTON_TYPE_NEUTRAL]: "bg-gray-100 enabled:hover:bg-gray-200",
+            [BUTTON_TYPE_DANGER]: "bg-red-100 enabled:hover:bg-red-600 text-white",
+        },
+    },
+});
+</script>
+
+<script setup>
+import {isButtonTypeValid} from "@/validators";
+
+defineProps({
     type: {
         type: String,
         default: BUTTON_TYPE_NEUTRAL,
-        validator: isButtonTypeValid
+        validator: isButtonTypeValid,
     },
-});
-
-const BaseButtonClasses = computed(() => {
-    return cva("rounded p-3 disabled:cursor-not-allowed disabled:opacity-50", {
-        variants: {
-            type: {
-                [BUTTON_TYPE_NEUTRAL]: "bg-gray-100 enabled:hover:bg-gray-200",
-                [BUTTON_TYPE_DANGER]: "bg-red-100 enabled:hover:bg-red-600 text-white",
-            },
-        },
-    })({type: props.type});
 });
 </script>
