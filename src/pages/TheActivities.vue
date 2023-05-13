64d99b85b41b211332b1aa07bede4849c0642329
Yevhenii Volosiuk <volosiukweb@gmail.com>
2023-05-13 17:21:26 +0200

    added form structure

--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -8,22 +8,40 @@
                 @delete="emit('deleteActivity', activity)"
             />
         </ul>
+
+        <form @submit.prevent="emit('createActivity', newActivity)" class="sticky bottom-[57px] flex gap-2 border-t bg-white p-4">
+            <input
+                :value="newActivity"
+                @input="newActivity = $event.target.value"
+                type="text"
+                class="w-full rounded border px-4 text-xl"
+                placeholder="Activity name"
+            />
+            <BaseButton>
+                <PlusIcon class="h-8" />
+            </BaseButton>
+        </form>
     </div>
 </template>
 
 <script setup>
+import {PlusIcon} from "@heroicons/vue/24/outline";
 import ActivityItem from "@/components/ActivityItem.vue";
-import { validateActivities, isActivityValid } from "@/validators";
+import BaseButton from "@/components/ui/BaseButton.vue";
+import {validateActivities, isActivityValid} from "@/validators";
 
 defineProps({
     activities: {
         type: Array,
         required: true,
-        validate: validateActivities
+        validate: validateActivities,
     },
 });
 
 const emit = defineEmits({
-    deleteActivity: isActivityValid
+    createActivity: isActivityValid,
+    deleteActivity: isActivityValid,
 });
+
+let newActivity = "";
 </script>
