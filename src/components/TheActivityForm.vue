fbe28364e47460c13ffc61a1237fa5c88be4a92d
Yevhenii Volosiuk <volosiukweb@gmail.com>
2023-05-16 00:47:25 +0200

    refactored form

--- src/components/TheActivityForm.vue
+++ src/components/TheActivityForm.vue
@@ -4,30 +4,31 @@
         class="sticky bottom-[57px] flex gap-2 border-t bg-white p-4"
     >
         <input
-            :value="activity"
-            @input="activity = $event.target.value"
+            v-model="activity"
             type="text"
             class="w-full rounded border px-4 text-xl"
             placeholder="Activity name"
         />
-        <BaseButton>
+        <BaseButton :disabled="activity.trim() === ''">
             <PlusIcon class="h-8" />
         </BaseButton>
     </form>
 </template>
 
 <script setup>
+import {ref} from "vue";
 import {PlusIcon} from "@heroicons/vue/24/outline";
 import BaseButton from "@/components/ui/BaseButton.vue";
-import { isActivityValid } from "@/validators";
+import {isActivityValid} from "@/validators";
 
 const emit = defineEmits({
     submit: isActivityValid,
 });
 
-let activity = "";
+const activity = ref("");
 
 function submit() {
-    emit('submit', activity)
+    emit("submit", activity.value);
+    activity.value = "";
 }
 </script>
