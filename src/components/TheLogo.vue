3cf5c597d4796b71c0c0baf63d8fab5b794c36d7
Yevhenii Volosiuk <volosiukweb@gmail.com>
2023-05-25 23:08:35 +0200

    add no state component for activities

--- src/components/TheLogo.vue
+++ src/components/TheLogo.vue
@@ -1,12 +1,13 @@
 <template>
-    <a :href="`#${PAGE_TIMELINE}`"
+    <a
+        class="relative w-48 h-10"
+        :href="`#${PAGE_TIMELINE}`"
         ><img
             src="../assets/images/logo.png"
             alt="Logo"
-            class="h-full w-48"
+            class="absolute left-0 top-0 h-full w-full bg-cover object-cover"
     /></a>
 </template>
-
 <script setup>
 import {PAGE_TIMELINE} from "@/constants";
 </script>
--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-        <ul class="divide-y">
+    <div class="flex grow flex-col">
+        <ul
+            v-if="activities.length"
+            class="grow divide-y"
+        >
             <ActivityItem
                 v-for="activity in activities"
                 :key="activity"
@@ -8,15 +11,16 @@
                 @delete="emit('deleteActivity', activity)"
             />
         </ul>
-
-        <TheActivityForm @submit="emit('createActivity', $event)"/>
+        <TheActivitiesEmptyState v-else />
+        <TheActivityForm @submit="emit('createActivity', $event)" />
     </div>
 </template>
 
 <script setup>
 import ActivityItem from "@/components/ActivityItem.vue";
-import { validateActivities, isActivityValid } from "@/validators";
-import TheActivityForm from '@/components/TheActivityForm.vue';
+import {validateActivities, isActivityValid} from "@/validators";
+import TheActivityForm from "@/components/TheActivityForm.vue";
+import TheActivitiesEmptyState from "@/components/TheActivitiesEmptyState.vue";
 
 defineProps({
     activities: {
@@ -30,6 +34,4 @@ const emit = defineEmits({
     createActivity: isActivityValid,
     deleteActivity: isActivityValid,
 });
-
-
 </script>
